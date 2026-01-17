5037eac261600e069c9242d80216bcd2a005c175
Fantamomo <git@fantamomo.com>
2026-01-17 18:04:54 +0100

    feat: add `FixWireTask` to task set and remove unused `common` property
    - Register `FixWireTask` in the set of available tasks for assignment.
    - Remove the unused `common` property from the `Task` interface.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

--- src/main/kotlin/com/fantamomo/mc/amongus/task/Task.kt
+++ src/main/kotlin/com/fantamomo/mc/amongus/task/Task.kt
@@ -1,6 +1,7 @@
 package com.fantamomo.mc.amongus.task
 
 import com.fantamomo.mc.amongus.player.AmongUsPlayer
+import com.fantamomo.mc.amongus.task.tasks.FixWireTask
 import com.fantamomo.mc.amongus.task.tasks.GarbageTask
 import com.fantamomo.mc.amongus.task.tasks.NumbersTask
 import com.fantamomo.mc.amongus.task.tasks.StartReaktorTask
@@ -8,8 +9,6 @@ import com.fantamomo.mc.amongus.task.tasks.StartReaktorTask
 interface Task<T : Task<T, A>, A : AssignedTask<T, A>> {
     val id: String
     val type: TaskType
-    val common: Boolean
-        get() = false
 
     fun assignTo(player: AmongUsPlayer): A?
 
@@ -17,7 +16,8 @@ interface Task<T : Task<T, A>, A : AssignedTask<T, A>> {
         val tasks = setOf<Task<*, *>>(
             GarbageTask,
             NumbersTask,
-            StartReaktorTask
+            StartReaktorTask,
+            FixWireTask
         )
     }
 }
