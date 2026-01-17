df2179237001d117de57874784063309d476b74f
Fantamomo <git@fantamomo.com>
2026-01-17 09:14:11 +0100

    feat: add `GuiAssignedTask` for GUI-based assigned tasks
    - Introduce `GuiAssignedTask` abstract class with inventory handling.
    - Add support for inventory setup, start, stop, and click event handling.
    - Include utility methods for determining middle and border item slots.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

---
+++ src/main/kotlin/com/fantamomo/mc/amongus/task/GuiAssignedTask.kt
@@ -0,0 +1,75 @@
+package com.fantamomo.mc.amongus.task
+
+import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.inventory.Inventory
+import org.bukkit.inventory.InventoryHolder
+
+abstract class GuiAssignedTask<T : Task<T, A>, A : GuiAssignedTask<T, A>> : AssignedTask<T, A>, InventoryHolder {
+    protected abstract val inv: Inventory
+
+    override fun getInventory() = inv
+
+    override fun stop() {
+        inv.close()
+        inv.clear()
+    }
+
+    override fun start() {
+        player.player?.openInventory(inv)
+        setupInventory()
+    }
+
+    open fun setupInventory() {}
+
+    open fun onInventoryClose() {
+        stop()
+    }
+
+    abstract fun onInventoryClick(event: InventoryClickEvent)
+
+    companion object {
+        fun getMiddleItemSlots(size: Int): List<Int> {
+            val result = mutableListOf<Int>()
+            val rows = size / 9
+
+            for (slot in 0 until size) {
+                val row = slot / 9
+                val col = slot % 9
+
+                val isBorder =
+                    row == 0 ||
+                            row == rows - 1 ||
+                            col == 0 ||
+                            col == 8
+
+                if (!isBorder) {
+                    result.add(slot)
+                }
+            }
+            return result
+        }
+
+        fun getBorderItemSlots(size: Int): List<Int> {
+            val result = mutableListOf<Int>()
+            val rows = size / 9
+
+            for (slot in 0 until size) {
+                val row = slot / 9
+                val col = slot % 9
+
+                val isBorder =
+                    row == 0 ||
+                            row == rows - 1 ||
+                            col == 0 ||
+                            col == 8
+
+                if (isBorder) {
+                    result.add(slot)
+                }
+            }
+
+            return result
+        }
+
+    }
+}
