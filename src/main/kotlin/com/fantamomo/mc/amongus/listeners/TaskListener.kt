19a372ccb932d393476ef7cdde1d4cd2b97a0b2f
Fantamomo <git@fantamomo.com>
2026-01-17 19:02:33 +0100

    feat: enhance task item movement and event handling
    - Introduce `isMoveable` function to identify moveable items via persistent data.
    - Add `onItemDrop` event to prevent dropping of moveable items.
    - Enhance `onInventoryClick` to restrict unintended item movements in task inventories.
    - Refine `onInventoryClose` to clean up moveable items from the player's inventory and cursor.
    - Improve `onItemDragEvent` to validate item dragging in task inventories.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

--- src/main/kotlin/com/fantamomo/mc/amongus/listeners/TaskListener.kt
+++ src/main/kotlin/com/fantamomo/mc/amongus/listeners/TaskListener.kt
@@ -2,6 +2,7 @@ package com.fantamomo.mc.amongus.listeners
 
 import com.fantamomo.mc.amongus.player.PlayerManager
 import com.fantamomo.mc.amongus.task.GuiAssignedTask
+import com.fantamomo.mc.amongus.task.GuiAssignedTask.Companion.MOVEABLE_ITEM_KEY
 import org.bukkit.entity.Player
 import org.bukkit.event.Event
 import org.bukkit.event.EventHandler
@@ -10,9 +11,15 @@ import org.bukkit.event.Listener
 import org.bukkit.event.block.Action
 import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.event.inventory.InventoryCloseEvent
+import org.bukkit.event.inventory.InventoryDragEvent
+import org.bukkit.event.player.PlayerDropItemEvent
 import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.inventory.ItemStack
+import org.bukkit.persistence.PersistentDataType
 
 object TaskListener : Listener {
+    private fun ItemStack.isMoveable() = persistentDataContainer.has(MOVEABLE_ITEM_KEY, PersistentDataType.BYTE)
+
     @EventHandler(priority = EventPriority.HIGHEST)
     fun onPlayerInteract(event: PlayerInteractEvent) {
         if (event.useInteractedBlock() == Event.Result.DENY || event.action == Action.PHYSICAL) return
@@ -28,9 +35,23 @@ object TaskListener : Listener {
     fun onInventoryClick(event: InventoryClickEvent) {
         if (event.isCancelled) return
         val player = event.whoClicked as? Player ?: return
-        val amongUsPlayer = PlayerManager.getPlayer(player) ?: return
-        val task = event.clickedInventory?.holder as? GuiAssignedTask<*, *> ?: return
-        event.isCancelled = true
+        PlayerManager.getPlayer(player) ?: return
+        val currentItem = event.currentItem
+        val cursorItem = event.cursor
+        val task = event.clickedInventory?.holder as? GuiAssignedTask<*, *>
+        if (task == null) {
+            if (!event.isShiftClick) return
+            if (event.view.topInventory is GuiAssignedTask<*, *>) {
+                event.isCancelled = true
+            }
+            return
+        }
+        if (!(cursorItem.isMoveable() && currentItem?.isEmpty != false) &&
+            !(currentItem?.isMoveable() == true && cursorItem.isEmpty) &&
+            !(currentItem?.isMoveable() == true && cursorItem.isMoveable())
+        ) {
+            event.isCancelled = true
+        }
         task.onInventoryClick(event)
     }
 
@@ -38,8 +59,35 @@ object TaskListener : Listener {
     fun onInventoryClose(event: InventoryCloseEvent) {
         if (event.reason == InventoryCloseEvent.Reason.PLUGIN) return
         val player = event.player as? Player ?: return
-        val amongUsPlayer = PlayerManager.getPlayer(player) ?: return
+        PlayerManager.getPlayer(player) ?: return
         val task = event.inventory.holder as? GuiAssignedTask<*, *> ?: return
         task.onInventoryClose()
+        player.inventory.forEachIndexed { index, stack ->
+            if (stack?.isMoveable() == true) player.inventory.setItem(index, null)
+        }
+        if (player.itemOnCursor.isMoveable()) {
+            player.setItemOnCursor(null)
+        }
+    }
+
+    @EventHandler
+    fun onItemDrop(event: PlayerDropItemEvent) {
+        if (event.itemDrop.itemStack.isMoveable()) {
+            event.isCancelled = true
+        }
+    }
+
+    @EventHandler
+    fun onItemDragEvent(event: InventoryDragEvent) {
+        if (event.isCancelled) return
+        val player = event.whoClicked as? Player ?: return
+        PlayerManager.getPlayer(player) ?: return
+        val cursorItem = event.cursor
+        val newItems = event.newItems
+        val task = event.inventory.holder as GuiAssignedTask<*, *>
+        if (cursorItem?.isMoveable() != true && !newItems.any { it.value.isMoveable() }) {
+            event.isCancelled = true
+        }
+        task.onInventoryDrag(event)
     }
 }
