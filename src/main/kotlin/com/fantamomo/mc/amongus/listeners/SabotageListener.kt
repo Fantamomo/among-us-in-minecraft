76a742c47258a59f2f236d5ca2dcc60d39eae841
Fantamomo <git@fantamomo.com>
2026-01-18 19:01:49 +0100

    feat: add player death handling in sabotage interactions
    - Prevent dead players from interacting with sabotage mechanisms.
    - Display death-related messages during invalid interactions.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

--- src/main/kotlin/com/fantamomo/mc/amongus/listeners/SabotageListener.kt
+++ src/main/kotlin/com/fantamomo/mc/amongus/listeners/SabotageListener.kt
@@ -3,7 +3,10 @@ package com.fantamomo.mc.amongus.listeners
 import com.fantamomo.mc.amongus.player.PlayerManager
 import com.fantamomo.mc.amongus.sabotage.CommunicationsSabotage
 import com.fantamomo.mc.amongus.sabotage.LightsSabotage
+import com.fantamomo.mc.amongus.sabotage.SeismicStabilizersSabotage
 import com.fantamomo.mc.amongus.util.isSameBlockPosition
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.title.TitlePart
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
@@ -20,12 +23,21 @@ object SabotageListener : Listener {
         when (sabotage) {
             is LightsSabotage -> {
                 val targetBlock = event.clickedBlock ?: return
-                sabotage.onLightLeverFlip(targetBlock.location)
+                sabotage.onLightLeverFlip(targetBlock.location, amongUsPlayer)
             }
             is CommunicationsSabotage -> {
                 val targetBlock = event.clickedBlock ?: return
                 if (sabotage.position.isSameBlockPosition(targetBlock.location)) {
                     sabotage.onPlayerInteract(amongUsPlayer)
+                }
+            }
+            is SeismicStabilizersSabotage -> {
+                val targetBlock = event.clickedBlock ?: return
+                if (sabotage.seismicStabilizers1 == targetBlock || sabotage.seismicStabilizers2 == targetBlock) {
+                    player.run {
+                        sendTitlePart(TitlePart.SUBTITLE, Component.translatable("sabotage.subtitle.dead"))
+                        sendTitlePart(TitlePart.TITLE, Component.translatable("sabotage.title.dead"))
+                    }
                 }
             }
         }
