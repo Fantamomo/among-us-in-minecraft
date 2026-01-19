34f4c9c7298f6acdde6e4b54f1a1ad0b741393a0
Fantamomo <git@fantamomo.com>
2026-01-19 21:56:17 +0100

    feat: add `BlockReason` enum for ability restrictions
    - Introduce `BlockReason` enum to define reasons for blocking abilities: IN_MEETING, SABOTAGE, IN_VENT, DEAD, NOT_SNEAKING, LIMIT_REACHED, CUSTOM.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

---
+++ src/main/kotlin/com/fantamomo/mc/amongus/ability/builder/BlockReason.kt
@@ -0,0 +1,11 @@
+package com.fantamomo.mc.amongus.ability.builder
+
+enum class BlockReason {
+    IN_MEETING,
+    SABOTAGE,
+    IN_VENT,
+    DEAD,
+    NOT_SNEAKING,
+    LIMIT_REACHED,
+    CUSTOM
+}
