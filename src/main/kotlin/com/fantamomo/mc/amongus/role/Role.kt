49a37123fb8d7b7a239a1112acf48790a6a9f0df
Fantamomo <git@fantamomo.com>
2026-01-20 20:37:57 +0100

    feat: introduce `ImposterRole` with abilities and team association
    - Add `ImposterRole` to define imposter behavior and properties.
    - Include support for `KillAbility`, `VentAbility`, and `SabotageAbility`.
    - Associate imposters with the `IMPOSTERS` team.
    - Ensure role is assignable to `AmongUsPlayer`.Role`
    
    - Introduce `team` property to associate a role with a specific team.
    - Add `canDoTask` default logic based on the team's ability to perform tasks.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

--- src/main/kotlin/com/fantamomo/mc/amongus/role/Role.kt
+++ src/main/kotlin/com/fantamomo/mc/amongus/role/Role.kt
@@ -5,6 +5,9 @@ import com.fantamomo.mc.amongus.player.AmongUsPlayer
 
 interface Role<R : Role<R, A>, A : AssignedRole<R, A>> {
     val id: String
+    val team: Team
+    val canDoTask: Boolean
+        get() = team.canDoTask
 
     /** A translation Key */
     val name: String
