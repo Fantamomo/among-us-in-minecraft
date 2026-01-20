4770f5e2c702442e2e2f5b7f128701afaeb6a0aa
Fantamomo <git@fantamomo.com>
2026-01-20 17:09:44 +0100

    feat: add utility for sending messages in commands
    - Introduce `sendMessage` extension function for `CommandContext` to simplify sending formatted text components.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

--- src/main/kotlin/com/fantamomo/mc/amongus/command/commandUtil.kt
+++ src/main/kotlin/com/fantamomo/mc/amongus/command/commandUtil.kt
@@ -1,7 +1,10 @@
 package com.fantamomo.mc.amongus.command
 
+import com.fantamomo.mc.adventure.text.KTextComponent
+import com.fantamomo.mc.adventure.text.textComponent
 import com.fantamomo.mc.brigadier.KtLiteralCommandBuilder
 import com.fantamomo.mc.brigadier.command
+import com.mojang.brigadier.context.CommandContext
 import com.mojang.brigadier.tree.LiteralCommandNode
 import io.papermc.paper.command.brigadier.CommandSourceStack
 import kotlin.contracts.ExperimentalContracts
@@ -14,4 +17,8 @@ typealias PaperCommand = KtLiteralCommandBuilder<CommandSourceStack>
 internal inline fun paperCommand(literal: String, block: PaperCommand.() -> Unit): LiteralCommandNode<CommandSourceStack> {
     contract { callsInPlace(block, InvocationKind.EXACTLY_ONCE) }
     return command(literal, block).build()
+}
+
+fun CommandContext<CommandSourceStack>.sendMessage(block: KTextComponent.() -> Unit) {
+    source.sender.sendMessage(textComponent(block))
 }
