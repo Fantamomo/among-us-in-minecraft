aef867f9be550369818884d4057909c8c11e2cd9
Fantamomo <git@fantamomo.com>
2026-01-20 18:11:15 +0100

    refactor: shift `GameAreaArgumentType` to use `parse` for type resolution
    - Replace `convert` with `parse` method for streamlined parsing using `StringReader`.
    - Improve error handling with `createWithContext` for invalid game areas.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

--- src/main/kotlin/com/fantamomo/mc/amongus/command/arguments/GameAreaArgumentType.kt
+++ src/main/kotlin/com/fantamomo/mc/amongus/command/arguments/GameAreaArgumentType.kt
@@ -3,6 +3,7 @@ package com.fantamomo.mc.amongus.command.arguments
 import com.fantamomo.mc.amongus.area.GameArea
 import com.fantamomo.mc.amongus.area.GameAreaManager
 import com.mojang.brigadier.LiteralMessage
+import com.mojang.brigadier.StringReader
 import com.mojang.brigadier.arguments.StringArgumentType
 import com.mojang.brigadier.context.CommandContext
 import com.mojang.brigadier.exceptions.DynamicCommandExceptionType
@@ -11,16 +12,17 @@ import com.mojang.brigadier.suggestion.SuggestionsBuilder
 import io.papermc.paper.command.brigadier.argument.CustomArgumentType
 import java.util.concurrent.CompletableFuture
 
-object GameAreaArgumentType : CustomArgumentType.Converted<GameArea, String> {
+object GameAreaArgumentType : CustomArgumentType<GameArea, String> {
 
     private val invalidGameArea: DynamicCommandExceptionType =
         DynamicCommandExceptionType { symbol: Any -> LiteralMessage("Invalid area: $symbol") }
 
-    override fun getNativeType(): StringArgumentType = StringArgumentType.word()
-
-    override fun convert(nativeType: String): GameArea {
-        return GameAreaManager.getArea(nativeType) ?: throw invalidGameArea.create(nativeType)
+    override fun parse(reader: StringReader): GameArea {
+        val input = reader.readUnquotedString()
+        return GameAreaManager.getArea(input) ?: throw invalidGameArea.createWithContext(reader, input)
     }
+
+    override fun getNativeType(): StringArgumentType = StringArgumentType.word()
 
     override fun <S : Any> listSuggestions(
         context: CommandContext<S>,
