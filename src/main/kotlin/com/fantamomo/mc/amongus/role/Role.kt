ab6ef61f1d3a822a3cd0552dd63e311e35b7a2b6
Fantamomo <git@fantamomo.com>
2026-01-21 08:33:53 +0100

    feat: add central `roles` registry in `Role` interface
    - Introduce a `roles` property to maintain a registry of all available roles.
    - Include `ImposterRole`, `CrewmateRole`, and `CameraManRole` in the registry.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

--- src/main/kotlin/com/fantamomo/mc/amongus/role/Role.kt
+++ src/main/kotlin/com/fantamomo/mc/amongus/role/Role.kt
@@ -2,6 +2,9 @@ package com.fantamomo.mc.amongus.role
 
 import com.fantamomo.mc.amongus.ability.Ability
 import com.fantamomo.mc.amongus.player.AmongUsPlayer
+import com.fantamomo.mc.amongus.role.crewmates.CameraManRole
+import com.fantamomo.mc.amongus.role.crewmates.CrewmateRole
+import com.fantamomo.mc.amongus.role.imposters.ImposterRole
 
 interface Role<R : Role<R, A>, A : AssignedRole<R, A>> {
     val id: String
@@ -11,8 +14,17 @@ interface Role<R : Role<R, A>, A : AssignedRole<R, A>> {
 
     /** A translation Key */
     val name: String
+        get() = "role.$id.name"
 
     val defaultAbilities: Set<Ability<*, *>>
 
     fun assignTo(player: AmongUsPlayer): A
+
+    companion object {
+        val roles: Set<Role<*, *>> = setOf(
+            ImposterRole,
+            CrewmateRole,
+            CameraManRole
+        )
+    }
 }
