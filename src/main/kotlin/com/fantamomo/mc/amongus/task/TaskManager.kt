87c25d2b01e0f1fd90c360c70108674b63b5f9a4
Fantamomo <git@fantamomo.com>
2026-01-21 16:13:21 +0100

    feat: add task completion and cleanup logic to `TaskManager`
    - Trigger `checkWin` when all tasks are completed.
    - Add `allTaskCompleted` method to evaluate task completion status.
    - Introduce `end` method to clean up boss bars and task states.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

--- src/main/kotlin/com/fantamomo/mc/amongus/task/TaskManager.kt
+++ src/main/kotlin/com/fantamomo/mc/amongus/task/TaskManager.kt
@@ -21,6 +21,7 @@ import net.kyori.adventure.title.TitlePart
 import org.bukkit.Color
 import org.bukkit.Location
 import org.bukkit.entity.BlockDisplay
+import org.bukkit.entity.Player
 import org.bukkit.persistence.PersistentDataType
 
 class TaskManager(val game: Game) {
@@ -87,6 +88,7 @@ class TaskManager(val game: Game) {
         AmongUs.server.scheduler.runTask(AmongUs) { ->
             removeMoveableItems(task.player)
         }
+        if (allTaskCompleted()) game.checkWin()
     }
 
     fun <T> completeOneTaskStep(task: T) where T : Steppable, T : AssignedTask<*, *> {
@@ -164,6 +166,8 @@ class TaskManager(val game: Game) {
         tasks.remove(player)
     }
 
+    fun allTaskCompleted(): Boolean = tasks.values.all { tasks -> tasks.all { it.completed || it.fake } }
+
     fun start() {
         val longTasksCount = game.settings[SettingsKey.TASK_LONG]
         val shortTasksCount = game.settings[SettingsKey.TASK_SHORT]
@@ -183,6 +187,14 @@ class TaskManager(val game: Game) {
             val common = commonTasks.randomListDistinct(commonTasksCount)
             assignTasks(player, long + short + common)
         }
+    }
+
+    internal fun end() {
+        for (viewer in bossbar.viewers().toList()) {
+            viewer as? Player ?: continue
+            viewer.hideBossBar(bossbar)
+        }
+        tasks.clear()
     }
 
     inner class RegisteredTask(
