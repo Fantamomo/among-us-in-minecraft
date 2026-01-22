cb03fc3bd219e336a0863e50f4bdca83a086e0f4
Fantamomo <git@fantamomo.com>
2026-01-22 10:57:04 +0100

    feat: register Engineer role in available roles list
    Signed-off-by: Fantamomo <git@fantamomo.com>

--- src/main/kotlin/com/fantamomo/mc/amongus/role/Role.kt
+++ src/main/kotlin/com/fantamomo/mc/amongus/role/Role.kt
@@ -4,6 +4,7 @@ import com.fantamomo.mc.amongus.ability.Ability
 import com.fantamomo.mc.amongus.player.AmongUsPlayer
 import com.fantamomo.mc.amongus.role.crewmates.CameraManRole
 import com.fantamomo.mc.amongus.role.crewmates.CrewmateRole
+import com.fantamomo.mc.amongus.role.crewmates.EngineerRole
 import com.fantamomo.mc.amongus.role.imposters.ImposterRole
 
 interface Role<R : Role<R, A>, A : AssignedRole<R, A>> {
@@ -24,7 +25,8 @@ interface Role<R : Role<R, A>, A : AssignedRole<R, A>> {
         val roles: Set<Role<*, *>> = setOf(
             ImposterRole,
             CrewmateRole,
-            CameraManRole
+            CameraManRole,
+            EngineerRole
         )
     }
 }
