e6ea1b1b34fa1de90354f23d700c7d087e1ad67c
Fantamomo <git@fantamomo.com>
2026-01-26 15:56:28 +0100

    feat: add `TaskState` enum with color and number format mapping
    - Introduce `TaskState` enum to represent task statuses (`INCOMPLETE`, `IN_PROGRESS`, `COMPLETED`).
    - Add color coding and fixed number formatting for each state.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

---
+++ src/main/kotlin/com/fantamomo/mc/amongus/task/TaskState.kt
@@ -0,0 +1,20 @@
+package com.fantamomo.mc.amongus.task
+
+import io.papermc.paper.scoreboard.numbers.NumberFormat
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.format.TextColor
+
+enum class TaskState(
+    val color: TextColor,
+    numberFormat: String
+) {
+    INCOMPLETE(NamedTextColor.RED, "scoreboard.task.incomplete"),
+    IN_PROGRESS(NamedTextColor.YELLOW, "scoreboard.task.in_progress"),
+    COMPLETED(NamedTextColor.GREEN, "scoreboard.task.completed");
+
+    val numberFormat: NumberFormat = NumberFormat.fixed(Component.translatable(numberFormat).color(color))
+
+    operator fun component1() = color
+    operator fun component2() = numberFormat
+}
