bd5c7138cc6367714a702b7c0b89d4a369d07f42
Fantamomo <git@fantamomo.com>
2026-01-26 15:59:00 +0100

    feat(task): add bold styling to task state number format
    - Update `TaskState` enum to include bold text decoration in number format styling for enhanced visibility.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

--- src/main/kotlin/com/fantamomo/mc/amongus/task/TaskState.kt
+++ src/main/kotlin/com/fantamomo/mc/amongus/task/TaskState.kt
@@ -3,7 +3,9 @@ package com.fantamomo.mc.amongus.task
 import io.papermc.paper.scoreboard.numbers.NumberFormat
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.format.Style
 import net.kyori.adventure.text.format.TextColor
+import net.kyori.adventure.text.format.TextDecoration
 
 enum class TaskState(
     val color: TextColor,
@@ -13,7 +15,7 @@ enum class TaskState(
     IN_PROGRESS(NamedTextColor.YELLOW, "scoreboard.task.in_progress"),
     COMPLETED(NamedTextColor.GREEN, "scoreboard.task.completed");
 
-    val numberFormat: NumberFormat = NumberFormat.fixed(Component.translatable(numberFormat).color(color))
+    val numberFormat: NumberFormat = NumberFormat.fixed(Component.translatable(numberFormat, Style.style(color, TextDecoration.BOLD)))
 
     operator fun component1() = color
     operator fun component2() = numberFormat
