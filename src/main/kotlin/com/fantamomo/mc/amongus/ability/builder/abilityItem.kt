aab9b184c60d8e19676899939d6e500af0d517cd
Fantamomo <git@fantamomo.com>
2026-01-28 16:41:25 +0100

    refactor(ability): update `abilityItem` to support generic type parameters
    - Modify `abilityItem` method to use generic types `A` and `S` for `Ability` and `AssignedAbility`.
    - Improve type safety and alignment
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

--- src/main/kotlin/com/fantamomo/mc/amongus/ability/builder/abilityItem.kt
+++ src/main/kotlin/com/fantamomo/mc/amongus/ability/builder/abilityItem.kt
@@ -1,5 +1,6 @@
 package com.fantamomo.mc.amongus.ability.builder
 
+import com.fantamomo.mc.amongus.ability.Ability
 import com.fantamomo.mc.amongus.ability.AssignedAbility
 import com.fantamomo.mc.amongus.ability.item.AbilityItem
 import kotlin.contracts.ExperimentalContracts
@@ -7,12 +8,12 @@ import kotlin.contracts.InvocationKind
 import kotlin.contracts.contract
 
 @OptIn(ExperimentalContracts::class)
-fun AssignedAbility<*, *>.abilityItem(
+fun <A : Ability<A, S>, S : AssignedAbility<A, S>> S.abilityItem(
     id: String,
-    block: AbilityItemBuilder.() -> Unit
+    block: AbilityItemBuilder<A, S>.() -> Unit
 ): AbilityItem {
     contract { callsInPlace(block, InvocationKind.EXACTLY_ONCE) }
-    val builder = AbilityItemBuilder(this, id)
+    val builder = AbilityItemBuilder<A, S>(this, id)
     builder.block()
     return builder.build()
 }
