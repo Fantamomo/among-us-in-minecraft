144103f9c37430e48dc063efcd8635acf3be7e46
Fantamomo <git@fantamomo.com>
2026-01-28 16:40:52 +0100

    refactor(ability): update AbilityContext to use generic type parameters
    - Replace raw `AssignedAbility` type with generic type parameters `A` and `S`.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

--- src/main/kotlin/com/fantamomo/mc/amongus/ability/builder/AbilityContext.kt
+++ src/main/kotlin/com/fantamomo/mc/amongus/ability/builder/AbilityContext.kt
@@ -1,9 +1,10 @@
 package com.fantamomo.mc.amongus.ability.builder
 
+import com.fantamomo.mc.amongus.ability.Ability
 import com.fantamomo.mc.amongus.ability.AssignedAbility
 
-class AbilityContext(
-    val ability: AssignedAbility<*, *>
+class AbilityContext<A : Ability<A, S>, S : AssignedAbility<A, S>>(
+    val ability: S
 ) {
     val player = ability.player
     val game = player.game
