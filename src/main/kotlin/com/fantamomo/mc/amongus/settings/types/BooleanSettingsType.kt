d85c8d41ffa8bb1ccd15cad688da9743e6b86e58
Fantamomo <git@fantamomo.com>
2026-02-10 11:26:27 +0100

    feat(settings): enhance `BooleanSettingsType` with colored text representation
    - Add `componentRepresentation` to `BooleanSettingsType` for Adventure `Component` rendering with color-coded values.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

--- src/main/kotlin/com/fantamomo/mc/amongus/settings/types/BooleanSettingsType.kt
+++ src/main/kotlin/com/fantamomo/mc/amongus/settings/types/BooleanSettingsType.kt
@@ -3,9 +3,16 @@ package com.fantamomo.mc.amongus.settings.types
 import com.fantamomo.mc.amongus.settings.SettingsType
 import com.mojang.brigadier.arguments.ArgumentType
 import com.mojang.brigadier.arguments.BoolArgumentType
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
 import kotlin.reflect.KClass
 
 object BooleanSettingsType : SettingsType<Boolean> {
     override val type: KClass<Boolean> = Boolean::class
     override val argumentType: ArgumentType<Boolean> = BoolArgumentType.bool()
+
+    private val TRUE = Component.text("true", NamedTextColor.GREEN)
+    private val FALSE = Component.text("false", NamedTextColor.RED)
+
+    override fun componentRepresentation(value: Boolean) = if (value) TRUE else FALSE
 }
