7258759513add60fbc774cff2091904c6b9350a4
Fantamomo <git@fantamomo.com>
2026-02-11 12:51:15 +0100

    feat(settings): add `SettingsListener` for handling settings UI interactions
    - Introduce `SettingsListener` to manage player interactions with the `SettingsInventory`.
    - Cancel invalid inventory interactions and delegate valid ones to `SettingsInventory`'s logic.
    - Register `SettingsListener` in the global `Listeners` list for event handling.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

---
+++ src/main/kotlin/com/fantamomo/mc/amongus/listeners/SettingsListener.kt
@@ -0,0 +1,21 @@
+package com.fantamomo.mc.amongus.listeners
+
+import com.fantamomo.mc.amongus.settings.SettingsInventory
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
+import org.bukkit.event.Listener
+import org.bukkit.event.inventory.InventoryClickEvent
+
+object SettingsListener : Listener {
+    @EventHandler(priority = EventPriority.HIGH)
+    fun onInventoryInteract(event: InventoryClickEvent) {
+        val inventory = event.inventory
+        val holder = inventory.holder
+        if (holder !is SettingsInventory) return
+        event.isCancelled = true
+        val player = event.whoClicked as? Player ?: return
+        if (holder.owner.player != player) return
+        holder.onClick(event)
+    }
+}
