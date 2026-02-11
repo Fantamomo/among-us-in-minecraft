ad2b003b1f43887b7e40e031f79148649e445f98
Fantamomo <git@fantamomo.com>
2026-02-11 12:54:12 +0100

    feat(settings): add item and interaction support to `DurationSettingsType` and `IntSettingsType`
    - Implement `itemRepresentation` to map values to `ItemStack`.
    - Add `onItemClick` method placeholders for interactive settings modification.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

--- src/main/kotlin/com/fantamomo/mc/amongus/settings/types/DurationSettingsType.kt
+++ src/main/kotlin/com/fantamomo/mc/amongus/settings/types/DurationSettingsType.kt
@@ -2,11 +2,18 @@ package com.fantamomo.mc.amongus.settings.types
 
 import com.fantamomo.mc.amongus.command.arguments.DurationArgumentType
 import com.fantamomo.mc.amongus.settings.SettingsType
+import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
 import kotlin.time.Duration
 
 class DurationSettingsType(min: Duration?, max: Duration?) : SettingsType<Duration> {
     override val type = Duration::class
     override val argumentType = DurationArgumentType(min, max)
+    override fun itemRepresentation(value: Duration) = ItemStack(Material.CLOCK)
+
+    override fun onItemClick(current: Duration): Duration {
+        TODO("Not yet implemented")
+    }
 
     companion object {
         val positive = DurationSettingsType(Duration.ZERO, null)
--- src/main/kotlin/com/fantamomo/mc/amongus/settings/types/IntSettingsType.kt
+++ src/main/kotlin/com/fantamomo/mc/amongus/settings/types/IntSettingsType.kt
@@ -3,6 +3,8 @@ package com.fantamomo.mc.amongus.settings.types
 import com.fantamomo.mc.amongus.settings.SettingsType
 import com.mojang.brigadier.arguments.ArgumentType
 import com.mojang.brigadier.arguments.IntegerArgumentType
+import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
 
 class IntSettingsType(
     val min: Int? = null,
@@ -10,6 +12,11 @@ class IntSettingsType(
 ) : SettingsType<Int> {
     override val type = Int::class
     override val argumentType: ArgumentType<Int> = IntegerArgumentType.integer(min ?: Int.MIN_VALUE, max ?: Int.MAX_VALUE)
+    override fun itemRepresentation(value: Int) = ItemStack(Material.STICK)
+
+    override fun onItemClick(current: Int): Int {
+        TODO("Not yet implemented")
+    }
 
     companion object {
         val positive = IntSettingsType(0, null)
