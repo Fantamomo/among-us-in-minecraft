c3749e547a1d1d4dcff17dad3961205185ed0c5b
Fantamomo <git@fantamomo.com>
2026-02-11 12:53:14 +0100

    feat(util): add `MaterialProvider` implementation to enums
    - Implement `MaterialProvider` interface in `TaskBarUpdateEnum` and `DistanceEnum`.
    - Add material abstraction with corresponding `Material` values for each enum constant.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

--- src/main/kotlin/com/fantamomo/mc/amongus/util/data/TaskBarUpdateEnum.kt
+++ src/main/kotlin/com/fantamomo/mc/amongus/util/data/TaskBarUpdateEnum.kt
@@ -1,13 +1,16 @@
 package com.fantamomo.mc.amongus.util.data
 
 import com.fantamomo.mc.amongus.util.Colored
+import com.fantamomo.mc.amongus.util.MaterialProvider
 import net.kyori.adventure.text.format.NamedTextColor
 import net.kyori.adventure.text.format.TextColor
+import org.bukkit.Material
 
-enum class TaskBarUpdateEnum(private val color: TextColor) : Colored{
-    IMMEDIATELY(NamedTextColor.GREEN),
-    MEETING(NamedTextColor.YELLOW),
-    NONE(NamedTextColor.RED);
+enum class TaskBarUpdateEnum(private val color: TextColor, private val material: Material) : Colored, MaterialProvider {
+    IMMEDIATELY(NamedTextColor.GREEN, Material.GREEN_WOOL),
+    MEETING(NamedTextColor.YELLOW, Material.YELLOW_WOOL),
+    NONE(NamedTextColor.RED, Material.RED_WOOL);
 
     override fun color() = color
+    override fun material() = material
 }
