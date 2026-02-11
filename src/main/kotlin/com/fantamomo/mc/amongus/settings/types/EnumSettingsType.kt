441cac52f8812d7610e07410e6edcf7a7c62082c
Fantamomo <git@fantamomo.com>
2026-02-11 12:53:32 +0100

    feat(settings): enhance `EnumSettingsType` with item and interaction support
    - Add `itemRepresentation` for rendering enum values as `ItemStack`.
    - Implement `onItemClick` to cycle through enum values interactively.
    - Adjust `EnumArgumentType` to expose `values` for internal usage.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

--- src/main/kotlin/com/fantamomo/mc/amongus/settings/types/EnumSettingsType.kt
+++ src/main/kotlin/com/fantamomo/mc/amongus/settings/types/EnumSettingsType.kt
@@ -3,8 +3,11 @@ package com.fantamomo.mc.amongus.settings.types
 import com.fantamomo.mc.amongus.command.arguments.EnumArgumentType
 import com.fantamomo.mc.amongus.settings.SettingsType
 import com.fantamomo.mc.amongus.util.Colored
+import com.fantamomo.mc.amongus.util.MaterialProvider
 import com.mojang.brigadier.arguments.ArgumentType
 import net.kyori.adventure.text.Component
+import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
 import kotlin.reflect.KClass
 
 class EnumSettingsType<E : Enum<E>> private constructor(override val type: KClass<E>) : SettingsType<E> {
@@ -18,4 +21,13 @@ class EnumSettingsType<E : Enum<E>> private constructor(override val type: KClas
     override fun componentRepresentation(value: E) =
         if (value is Colored) Component.text(value.name, value.color())
         else Component.text(value.name)
+
+    override fun itemRepresentation(value: E) = ItemStack((value as? MaterialProvider)?.material() ?: Material.STONE)
+
+    override fun onItemClick(current: E): E {
+        val enumArgument = argumentType as EnumArgumentType<E>
+        val values = enumArgument.values
+        val index = values.indexOf(current) + 1
+        return values[index % values.size]
+    }
 }
