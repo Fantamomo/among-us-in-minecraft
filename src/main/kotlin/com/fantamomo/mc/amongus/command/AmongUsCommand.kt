6d906e550e72524aba2109c023e4154a08db4bcb
Fantamomo <git@fantamomo.com>
2026-02-12 21:32:20 +0100

    feat(commands): add `color` command for player color management
    - Implement `/amongus color` command to set or randomize player colors.
    - Add support for targeting specific players and enforcing color changes.
    - Ensure compatibility with lobby-only phase and avoid color conflicts.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

--- src/main/kotlin/com/fantamomo/mc/amongus/command/AmongUsCommand.kt
+++ src/main/kotlin/com/fantamomo/mc/amongus/command/AmongUsCommand.kt
@@ -2,17 +2,130 @@ package com.fantamomo.mc.amongus.command
 
 import com.fantamomo.mc.adventure.text.args
 import com.fantamomo.mc.adventure.text.translatable
+import com.fantamomo.mc.amongus.command.arguments.PlayerColorArgumentType
+import com.fantamomo.mc.amongus.game.GamePhase
+import com.fantamomo.mc.amongus.languages.component
 import com.fantamomo.mc.amongus.languages.numeric
 import com.fantamomo.mc.amongus.languages.string
+import com.fantamomo.mc.amongus.player.PlayerColor
 import com.fantamomo.mc.amongus.player.PlayerManager
 import com.fantamomo.mc.amongus.player.PlayerStatistics
 import com.fantamomo.mc.amongus.statistics.*
 import com.fantamomo.mc.brigadier.*
+import com.mojang.brigadier.arguments.BoolArgumentType
+import io.papermc.paper.command.brigadier.CommandSourceStack
+import io.papermc.paper.command.brigadier.argument.ArgumentTypes
+import io.papermc.paper.command.brigadier.argument.resolvers.selector.PlayerSelectorArgumentResolver
 import org.bukkit.entity.Player
 import kotlin.time.Duration.Companion.milliseconds
 import kotlin.uuid.toKotlinUuid
 
 val AmongUsCommand = paperCommand("amongus") {
+    statsCommand()
+    colorCommand()
+}
+
+private fun PaperCommand.colorCommand() {
+    literal("color") {
+        argument("color", PlayerColorArgumentType) {
+            argument("target", ArgumentTypes.player()) {
+                requires { sender.hasPermission(Permissions.SET_PLAYER_COLOR_OTHER) }
+                argument("force", BoolArgumentType.bool()) {
+                    requires { sender.hasPermission(Permissions.SET_PLAYER_COLOR_FORCE) }
+                    setColorCommand()
+                }
+                setColorCommand()
+            }
+            setColorCommand()
+        }
+        literal("random") {
+            argument("target", ArgumentTypes.player()) {
+                requires { sender.hasPermission(Permissions.SET_PLAYER_COLOR_OTHER) }
+                setColorCommand()
+            }
+            setColorCommand()
+        }
+    }
+}
+
+private fun KtCommandBuilder<CommandSourceStack, *>.setColorCommand() = execute {
+    val resolver = optionalArg<PlayerSelectorArgumentResolver>("target")
+    var target = resolver?.resolve(source)?.firstOrNull()
+    if (target == null) {
+        target = source.sender as? Player
+        if (target == null) {
+            sendMessage {
+                translatable("command.error.set_color.not_player")
+            }
+            return@execute 0
+        }
+    }
+    val amongUsPlayer = PlayerManager.getPlayer(target)
+    if (amongUsPlayer == null) {
+        sendMessage {
+            if (target === source.sender) {
+                translatable("command.error.set_color.not_joined")
+            } else {
+                translatable("command.error.set_color.not_joined.other") {
+                    args {
+                        string("player", target.name)
+                    }
+                }
+            }
+        }
+        return@execute 0
+    }
+
+    val game = amongUsPlayer.game
+    if (game.phase != GamePhase.LOBBY) {
+        sendMessage {
+            translatable("command.error.set_color.game_not_lobby")
+        }
+        return@execute 0
+    }
+    val color = optionalArg<PlayerColor>("color") ?: game.randomPlayerColor()
+    val force = optionalArg<Boolean>("force") == true
+
+    val uses = game.players.find { it.color == color }
+
+    if (uses != null && !force) {
+        sendMessage {
+            translatable("command.error.set_color.color_already_used") {
+                args {
+                    component("color", color.coloredName)
+                }
+            }
+        }
+        return@execute 0
+    }
+
+    amongUsPlayer.color = color
+    uses?.color = game.randomPlayerColor()
+
+    sendMessage {
+        if (uses == null) {
+            translatable(if (target === source.sender) "command.success.set_color" else "command.success.set_color.other") {
+                args {
+                    component("color", color.coloredName)
+                    string("player", target.name)
+                }
+            }
+        } else {
+            translatable(if (target === source.sender) "command.success.set_color.overridden" else "command.success.set_color.other.overridden") {
+                args {
+                    string("player", target.name)
+                    component("color", color.coloredName)
+                    string("other", uses.name)
+                    component("other_color", uses.color.coloredName)
+                }
+            }
+        }
+    }
+
+    SINGLE_SUCCESS
+}
+
+private fun PaperCommand.statsCommand() {
     literal("stats") {
         guard {
             val sender = source.sender as? Player
