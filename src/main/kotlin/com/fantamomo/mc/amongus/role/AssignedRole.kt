bc057b65679e91b4168f9acd37b4cd1412434515
Fantamomo <git@fantamomo.com>
2026-02-14 09:15:11 +0100

    feat(role): add lifecycle hooks to `AssignedRole`
    - Introduce `onGameStart`, `onGameEnd`, and `tick` methods to `AssignedRole` for role-specific lifecycle management.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

--- src/main/kotlin/com/fantamomo/mc/amongus/role/AssignedRole.kt
+++ src/main/kotlin/com/fantamomo/mc/amongus/role/AssignedRole.kt
@@ -5,4 +5,8 @@ import com.fantamomo.mc.amongus.player.AmongUsPlayer
 interface AssignedRole<R : Role<R, A>, A : AssignedRole<R, A>> {
     val definition: R
     val player: AmongUsPlayer
+
+    fun onGameStart() {}
+    fun onGameEnd() {}
+    fun tick() {}
 }
