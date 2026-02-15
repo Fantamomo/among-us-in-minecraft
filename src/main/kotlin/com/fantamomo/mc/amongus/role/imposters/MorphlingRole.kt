1dd8aa614f09f4d6fc98dc19003711efb48b2774
Fantamomo <git@fantamomo.com>
2026-02-15 18:43:40 +0100

    feat(role): implement `scoreboardLine` for MorphlingRole
    - Added `scoreboardLine` method to display the morphed player on the scoreboard.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

--- src/main/kotlin/com/fantamomo/mc/amongus/role/imposters/MorphlingRole.kt
+++ src/main/kotlin/com/fantamomo/mc/amongus/role/imposters/MorphlingRole.kt
@@ -1,7 +1,11 @@
 package com.fantamomo.mc.amongus.role.imposters
 
+import com.fantamomo.mc.adventure.text.args
+import com.fantamomo.mc.adventure.text.textComponent
+import com.fantamomo.mc.adventure.text.translatable
 import com.fantamomo.mc.amongus.ability.Ability
 import com.fantamomo.mc.amongus.ability.abilities.MorphAbility
+import com.fantamomo.mc.amongus.languages.string
 import com.fantamomo.mc.amongus.player.AmongUsPlayer
 import com.fantamomo.mc.amongus.role.AssignedRole
 import com.fantamomo.mc.amongus.role.Role
@@ -16,5 +20,15 @@ object MorphlingRole : Role<MorphlingRole, MorphlingRole.AssignedMorphlingRole> 
 
     class AssignedMorphlingRole(override val player: AmongUsPlayer) : AssignedRole<MorphlingRole, AssignedMorphlingRole> {
         override val definition = MorphlingRole
+
+        override fun scoreboardLine() = player.game.morphManager.getMorphedPlayer(player)?.let { morphedPlayer ->
+            textComponent {
+                translatable("role.morphling.scoreboard") {
+                    args {
+                        string("target", morphedPlayer.target.name)
+                    }
+                }
+            }
+        }
     }
 }
