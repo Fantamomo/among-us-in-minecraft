a5001b5c54ccf9e6aa5f6817df6b0ae4db667c9d
Fantamomo <git@fantamomo.com>
2026-02-15 18:43:47 +0100

    feat(role): implement `scoreboardLine` for PhantomRole
    - Added `scoreboardLine` method to display the remaining ghost time on the scoreboard.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

--- src/main/kotlin/com/fantamomo/mc/amongus/role/imposters/PhantomRole.kt
+++ src/main/kotlin/com/fantamomo/mc/amongus/role/imposters/PhantomRole.kt
@@ -1,11 +1,17 @@
 package com.fantamomo.mc.amongus.role.imposters
 
+import com.fantamomo.mc.adventure.text.args
+import com.fantamomo.mc.adventure.text.textComponent
+import com.fantamomo.mc.adventure.text.translatable
 import com.fantamomo.mc.amongus.ability.Ability
 import com.fantamomo.mc.amongus.ability.abilities.GhostFormAbility
+import com.fantamomo.mc.amongus.languages.string
 import com.fantamomo.mc.amongus.player.AmongUsPlayer
 import com.fantamomo.mc.amongus.role.AssignedRole
 import com.fantamomo.mc.amongus.role.Role
 import com.fantamomo.mc.amongus.role.Team
+import com.fantamomo.mc.amongus.util.toSmartString
+import kotlin.time.DurationUnit
 
 object PhantomRole : Role<PhantomRole, PhantomRole.AssignedPhantomRole> {
     override val id: String = "phantom"
@@ -16,5 +22,15 @@ object PhantomRole : Role<PhantomRole, PhantomRole.AssignedPhantomRole> {
 
     class AssignedPhantomRole(override val player: AmongUsPlayer) : AssignedRole<PhantomRole, AssignedPhantomRole> {
         override val definition = PhantomRole
+
+        override fun scoreboardLine() = player.game.ghostFormManager.getGhostPlayer(player)?.let { ghostPlayer ->
+            textComponent {
+                translatable("role.phantom.scoreboard") {
+                    args {
+                        string("time", ghostPlayer.remainingTime.toSmartString(DurationUnit.SECONDS))
+                    }
+                }
+            }
+        }
     }
 }
