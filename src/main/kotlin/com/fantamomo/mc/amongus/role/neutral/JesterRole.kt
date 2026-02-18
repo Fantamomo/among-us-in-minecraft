d42556edde02f52ef28bf64c8a898ad31d12e0db
Fantamomo <git@fantamomo.com>
2026-02-18 16:23:29 +0100

    feat(role): add `JesterRole` as a new neutral role
    - Introduced `JesterRole` with neutral alignment and basic assignment logic.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

---
+++ src/main/kotlin/com/fantamomo/mc/amongus/role/neutral/JesterRole.kt
@@ -0,0 +1,19 @@
+package com.fantamomo.mc.amongus.role.neutral
+
+import com.fantamomo.mc.amongus.ability.Ability
+import com.fantamomo.mc.amongus.player.AmongUsPlayer
+import com.fantamomo.mc.amongus.role.AssignedRole
+import com.fantamomo.mc.amongus.role.Role
+import com.fantamomo.mc.amongus.role.Team
+
+object JesterRole : Role<JesterRole, JesterRole.AssignedJesterRole> {
+    override val id: String = "jester"
+    override val team: Team = Team.NEUTRAL(JesterRole)
+    override val defaultAbilities: Set<Ability<*, *>> = setOf()
+
+    override fun assignTo(player: AmongUsPlayer) = AssignedJesterRole(player)
+
+    class AssignedJesterRole(override val player: AmongUsPlayer) : AssignedRole<JesterRole, AssignedJesterRole>{
+        override val definition = JesterRole
+    }
+}
