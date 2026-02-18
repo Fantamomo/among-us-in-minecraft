4022aacf385d87b303c6189284edcf4ea9c7ce72
Fantamomo <git@fantamomo.com>
2026-02-18 16:23:56 +0100

    refactor(team): convert `Team` from enum to sealed class
    - Replaced enum with a sealed class for improved flexibility and extensibility.
    - Added support for neutral teams with dynamic role assignment.
    - Introduced robust validation for `Team` initialization.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

--- src/main/kotlin/com/fantamomo/mc/amongus/role/Team.kt
+++ src/main/kotlin/com/fantamomo/mc/amongus/role/Team.kt
@@ -4,18 +4,33 @@ import com.fantamomo.mc.amongus.role.crewmates.CrewmateRole
 import com.fantamomo.mc.amongus.role.imposters.ImposterRole
 import net.kyori.adventure.text.Component
 
-enum class Team {
-    CREWMATES,
-    IMPOSTERS;
+sealed class Team(val name: String, private val default: Role<*, *>?, val id: String = name) {
 
-    val canDoTask: Boolean
-        get() = this == CREWMATES
+    init {
+        require(id.isNotBlank()) { "Team ID cannot be blank" }
+        require(default != null || id != name) { "Default role must be provided for non-neutral teams" }
 
-    val description = Component.translatable("team.${name.lowercase()}")
+    }
+
+    open val canDoTask: Boolean
+        get() = this === CREWMATES
 
     val defaultRole: Role<*, *>
-        get() = when (this) {
-            CREWMATES -> CrewmateRole
-            IMPOSTERS -> ImposterRole
+        get() = default ?: throw IllegalStateException("$id team has no default role")
+
+    val description = Component.translatable("team.$id")
+
+    data object CREWMATES : Team("crewmates", CrewmateRole)
+    data object IMPOSTERS : Team("imposters", ImposterRole)
+    data class NEUTRAL(val role: Role<*, *>) : Team(role.id, null, "neutral.${role.id}") {
+        init {
+            teams.add(this)
         }
+    }
+
+    companion object {
+        private val teams: MutableSet<Team> = mutableSetOf(CREWMATES, IMPOSTERS)
+
+        fun getTeams(): Set<Team> = teams
+    }
 }
