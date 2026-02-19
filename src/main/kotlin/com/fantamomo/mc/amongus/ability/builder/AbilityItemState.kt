439ebc4d4d9abe1b5400af7b41f03aceefb411d9
Fantamomo <git@fantamomo.com>
2026-02-19 22:16:00 +0100

    feat(ability): add `createDefault` and improve `AbilityItemState` rendering logic
    - Introduced `createDefault` method for default state definition handling.
    - Added `abilityItemRenderer` for typed item rendering using `ItemType` and metadata.
    - Enhanced `AbilityItemState` with `COOLDOWN` specific rendering functionality.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

--- src/main/kotlin/com/fantamomo/mc/amongus/ability/builder/AbilityItemState.kt
+++ src/main/kotlin/com/fantamomo/mc/amongus/ability/builder/AbilityItemState.kt
@@ -1,7 +1,28 @@
 package com.fantamomo.mc.amongus.ability.builder
 
+import org.bukkit.inventory.ItemType
+import org.bukkit.inventory.meta.ItemMeta
+
 enum class AbilityItemState {
     ACTIVE,
     BLOCKED,
-    COOLDOWN
+    COOLDOWN;
+
+    open fun createDefault() = when (this) {
+        COOLDOWN -> AbilityItemStateDefinition().apply {
+            render = asItemStack
+        }
+        else -> AbilityItemStateDefinition()
+    }
+
+    companion object {
+        private const val COOLDOWN_DISPLAY_NAME = "ability.general.disabled.cooldown"
+
+        @Suppress("UnstableApiUsage")
+        private val abilityItemRenderer: AbilityItemRender<ItemMeta>.() -> Unit = {
+            itemType = ItemType.BARRIER
+            translationKey = COOLDOWN_DISPLAY_NAME
+        }
+        private val asItemStack = abilityItemRenderer::toItemStack
+    }
 }
