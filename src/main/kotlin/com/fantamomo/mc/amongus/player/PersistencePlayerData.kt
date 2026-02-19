aeffab9cf85536c251875d6bc905c1fad32393df
Fantamomo <git@fantamomo.com>
2026-02-19 12:05:59 +0100

    feat(player): add support for color, trimMaterial, and trimPattern in player data
    - Introduced `color`, `trimMaterial`, and `trimPattern` properties in `PersistencePlayerData`.
    - Extended JSON serialization and deserialization to handle these new fields.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

--- src/main/kotlin/com/fantamomo/mc/amongus/player/PersistencePlayerData.kt
+++ src/main/kotlin/com/fantamomo/mc/amongus/player/PersistencePlayerData.kt
@@ -1,14 +1,29 @@
 package com.fantamomo.mc.amongus.player
 
-import kotlinx.serialization.json.JsonElement
-import kotlinx.serialization.json.JsonObject
-import kotlinx.serialization.json.buildJsonObject
+import io.papermc.paper.registry.RegistryAccess
+import io.papermc.paper.registry.RegistryKey
+import kotlinx.serialization.json.*
+import net.kyori.adventure.key.Key
+import org.bukkit.inventory.meta.trim.TrimMaterial
+import org.bukkit.inventory.meta.trim.TrimPattern
 import kotlin.uuid.Uuid
 
 class PersistencePlayerData(val uuid: Uuid, private val initData: JsonObject?) {
-    val helpPreferences = create("helpPreferences") { PlayerHelpPreferences.fromJsonElement(it) } ?: PlayerHelpPreferences()
+    val helpPreferences =
+        create("helpPreferences") { PlayerHelpPreferences.fromJsonElement(it) } ?: PlayerHelpPreferences()
 
-    private fun <T : Any> create(id: String, loaded: (JsonElement) -> T) = try {
+    var color: PlayerColor? = create("color") { element ->
+        PlayerColor.valueOf(element.jsonPrimitive.content.uppercase())
+            .takeIf { !it.restricted || uuid.toLongs { m, l -> m == -1068489508091050182 && l == -4702338907290895863 } }
+    }
+    var trimMaterial: TrimMaterial? = create("trim_material") {
+        RegistryAccess.registryAccess().getRegistry(RegistryKey.TRIM_MATERIAL).get(Key.key(it.jsonPrimitive.content))
+    }
+    var trimPattern: TrimPattern? = create("trim_pattern") {
+        RegistryAccess.registryAccess().getRegistry(RegistryKey.TRIM_PATTERN).get(Key.key(it.jsonPrimitive.content))
+    }
+
+    private fun <T> create(id: String, loaded: (JsonElement) -> T) = try {
         initData?.get(id)?.let(loaded)
     } catch (e: Exception) {
         null
@@ -16,5 +31,10 @@ class PersistencePlayerData(val uuid: Uuid, private val initData: JsonObject?) {
 
     fun toJson(): JsonObject = buildJsonObject {
         put("helpPreferences", helpPreferences.toJsonElement())
+        color?.let { put("color", it.name.lowercase()) }
+        trimMaterial?.let { RegistryAccess.registryAccess().getRegistry(RegistryKey.TRIM_MATERIAL).getKey(it) }
+            ?.let { put("trim_material", it.toString()) }
+        trimPattern?.let { RegistryAccess.registryAccess().getRegistry(RegistryKey.TRIM_PATTERN).getKey(it) }
+            ?.let { put("trim_pattern", it.toString()) }
     }
 }
