eded9ea05b4dd488aa4614350cfc1ce8602ca407
Fantamomo <git@fantamomo.com>
2026-02-19 19:17:10 +0100

    feat(statistics): add `shouldSave` method to `Statistic` interface
    Signed-off-by: Fantamomo <git@fantamomo.com>

--- src/main/kotlin/com/fantamomo/mc/amongus/statistics/Statistic.kt
+++ src/main/kotlin/com/fantamomo/mc/amongus/statistics/Statistic.kt
@@ -3,6 +3,7 @@ package com.fantamomo.mc.amongus.statistics
 import kotlinx.serialization.json.JsonElement
 
 sealed interface Statistic {
+    fun shouldSave(): Boolean
     fun toData(): MutableMap<String, JsonElement>
 
     val id: String
