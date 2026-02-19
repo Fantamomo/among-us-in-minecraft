6afda7867d0022e66db9c417ef9bdca9b76e83ac
Fantamomo <git@fantamomo.com>
2026-02-19 19:14:33 +0100

    feat(role): add KillerRole marker interface
    - Introduced `KillerRole` interface for roles capable of killing players.
    - Supports integration with `PlayerStatistics` for kill-related data management.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

---
+++ src/main/kotlin/com/fantamomo/mc/amongus/role/marker/KillerRole.kt
@@ -0,0 +1,7 @@
+package com.fantamomo.mc.amongus.role.marker
+
+/**
+ * A marker interface for roles that can kill other players.
+ * This interface is used in [com.fantamomo.mc.amongus.player.PlayerStatistics] to manage statistics related to kills.
+ */
+interface KillerRole
