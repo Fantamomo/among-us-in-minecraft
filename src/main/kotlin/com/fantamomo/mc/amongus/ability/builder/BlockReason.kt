4e919020bc3c7ad9cacdce2e005ceac4cdca73ee
Fantamomo <git@fantamomo.com>
2026-02-20 08:32:19 +0100

    refactor(ability): add default block messages for `BlockReason` states using `Component`
    Signed-off-by: Fantamomo <git@fantamomo.com>

--- src/main/kotlin/com/fantamomo/mc/amongus/ability/builder/BlockReason.kt
+++ src/main/kotlin/com/fantamomo/mc/amongus/ability/builder/BlockReason.kt
@@ -1,13 +1,30 @@
 package com.fantamomo.mc.amongus.ability.builder
 
+import net.kyori.adventure.text.Component
+
 sealed interface BlockReason {
-    data object InMeeting : BlockReason
-    data object Sabotage : BlockReason
-    data object InVent : BlockReason
+    data object InMeeting : BlockReason {
+        override val defaultBlockMessage = Component.translatable("ability.general.disabled.in_meeting")
+    }
+
+    data object Sabotage : BlockReason {
+        override val defaultBlockMessage = Component.translatable("ability.general.disabled.sabotage")
+    }
+
+    data object InVent : BlockReason {
+        override val defaultBlockMessage = Component.translatable("ability.general.disabled.in_vent")
+    }
+
     data object Dead : BlockReason
     data object LimitReached : BlockReason
-    data object GhostForm : BlockReason
+    data object GhostForm : BlockReason {
+        override val defaultBlockMessage = Component.translatable("ability.general.disabled.ghost_form")
+    }
+
     data class Custom(val id: String) : BlockReason
+
+    val defaultBlockMessage: Component?
+        get() = null
 
     companion object {
         operator fun invoke(id: String) = Custom(id)
