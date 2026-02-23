04fbbc89663f703152695aa836a02c0b3da2281a
Fantamomo <git@fantamomo.com>
2026-02-23 18:08:51 +0100

    feat(modification): introduce framework for game modifications
    - Added a `Modification` interface to define game modifications with properties such as `id`, `name`, and `description`.
    - Introduced structures for assigning modifications to players using `randomModification` logic.
    - Registered initial modifications: `TorchModification`, `LaggyModification`, and `SmallModification`.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

---
+++ src/main/kotlin/com/fantamomo/mc/amongus/modification/Modification.kt
@@ -0,0 +1,35 @@
+package com.fantamomo.mc.amongus.modification
+
+import com.fantamomo.mc.amongus.modification.modifications.LaggyModification
+import com.fantamomo.mc.amongus.modification.modifications.SmallModification
+import com.fantamomo.mc.amongus.modification.modifications.TorchModification
+import com.fantamomo.mc.amongus.player.AmongUsPlayer
+import net.kyori.adventure.text.Component
+
+interface Modification<M : Modification<M, A>, A : AssignedModification<M, A>> {
+    val id: String
+
+    val name: Component
+        get() = Component.translatable("modification.$id.name")
+    val description: Component
+        get() = Component.translatable("modification.$id.description")
+
+    fun canAssignTo(player: AmongUsPlayer): Boolean = true
+
+    fun assignTo(player: AmongUsPlayer): A
+
+    companion object {
+        val modifications: List<Modification<*, *>> = listOf(
+            TorchModification,
+            LaggyModification,
+            SmallModification
+        )
+
+        private val sharedShuffledModification = modifications.toMutableList()
+
+        private fun sharedShuffledModification(): List<Modification<*, *>> = sharedShuffledModification.apply { shuffle() }
+
+        fun randomModification(player: AmongUsPlayer): AssignedModification<*, *>? =
+            sharedShuffledModification().find { it.canAssignTo(player) }?.assignTo(player)
+    }
+}
