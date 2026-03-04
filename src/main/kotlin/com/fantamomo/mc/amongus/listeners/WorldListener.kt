d9e0009e2665488e308d1fb37daf46b88611570e
Fantamomo <git@fantamomo.com>
2026-03-04 17:55:33 +0100

    feat(listeners): add `WorldListener` for managing world initialization
    - Introduced `WorldListener` to handle `WorldInitEvent` and associate game areas with their respective world folders.
    - Updated `Listeners` registry to include the new listener.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

---
+++ src/main/kotlin/com/fantamomo/mc/amongus/listeners/WorldListener.kt
@@ -0,0 +1,19 @@
+package com.fantamomo.mc.amongus.listeners
+
+import com.fantamomo.mc.amongus.AmongUs
+import com.fantamomo.mc.amongus.area.GameAreaManager
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.world.WorldInitEvent
+
+object WorldListener : Listener {
+    @EventHandler
+    fun onWorldLoad(event: WorldInitEvent) {
+        val world = event.world
+        val areas = GameAreaManager.getAreas(world)
+        val worldContainer = AmongUs.server.worldContainer.toPath()
+        areas.forEach { area ->
+            area.worldFolder = worldContainer.relativize(world.worldPath).toString()
+        }
+    }
+}
