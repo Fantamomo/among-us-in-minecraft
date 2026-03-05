da8f5ea78dc47c48a81f7f3c24c697c0e7747870
Fantamomo <git@fantamomo.com>
2026-03-05 11:51:11 +0100

    feat(abilities): add tooltip messages for block reasons
    - Introduced `tooltipMessage` to provide localized tooltips for block reasons such as meeting, sabotage, vent, dead, and ghost form.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

--- src/main/kotlin/com/fantamomo/mc/amongus/ability/builder/BlockReason.kt
+++ src/main/kotlin/com/fantamomo/mc/amongus/ability/builder/BlockReason.kt
@@ -5,25 +5,34 @@ import net.kyori.adventure.text.Component
 sealed interface BlockReason {
     data object InMeeting : BlockReason {
         override val defaultBlockMessage = Component.translatable("ability.general.disabled.in_meeting")
+        override val tooltipMessage = Component.translatable("ability.general.tooltip.in_meeting")
     }
 
     data object Sabotage : BlockReason {
         override val defaultBlockMessage = Component.translatable("ability.general.disabled.sabotage")
+        override val tooltipMessage = Component.translatable("ability.general.tooltip.sabotage")
     }
 
     data object InVent : BlockReason {
         override val defaultBlockMessage = Component.translatable("ability.general.disabled.in_vent")
+        override val tooltipMessage = Component.translatable("ability.general.tooltip.in_vent")
     }
 
-    data object Dead : BlockReason
+    data object Dead : BlockReason {
+        override val tooltipMessage = Component.translatable("ability.general.tooltip.dead")
+    }
     data object LimitReached : BlockReason
     data object GhostForm : BlockReason {
         override val defaultBlockMessage = Component.translatable("ability.general.disabled.ghost_form")
+        override val tooltipMessage = Component.translatable("ability.general.tooltip.ghost_form")
     }
 
     data class Custom(val id: String) : BlockReason
 
     val defaultBlockMessage: Component?
+        get() = null
+
+    val tooltipMessage: Component?
         get() = null
 
     companion object {
