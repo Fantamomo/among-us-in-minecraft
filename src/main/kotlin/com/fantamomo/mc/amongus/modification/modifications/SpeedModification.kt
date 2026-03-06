30e97a1169bd40338af7b0e75688a6749512bb32
Fantamomo <git@fantamomo.com>
2026-03-06 09:11:59 +0100

    feat(modification): introduce `SpeedModification` for player movement speed adjustment
    - Added `SpeedModification` implementation to modify player movement speed.
    - Utilized `AssignedAttributeModifier` for automatic lifecycle management of speed attribute.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

---
+++ src/main/kotlin/com/fantamomo/mc/amongus/modification/modifications/SpeedModification.kt
@@ -0,0 +1,27 @@
+package com.fantamomo.mc.amongus.modification.modifications
+
+import com.fantamomo.mc.amongus.AmongUs
+import com.fantamomo.mc.amongus.modification.AssignedAttributeModifier
+import com.fantamomo.mc.amongus.modification.Modification
+import com.fantamomo.mc.amongus.player.AmongUsPlayer
+import org.bukkit.NamespacedKey
+import org.bukkit.attribute.Attribute
+import org.bukkit.attribute.AttributeModifier
+
+object SpeedModification : Modification<SpeedModification, SpeedModification.AssignedSpeedModification> {
+    override val id: String = "speed"
+
+    override fun assignTo(player: AmongUsPlayer) = AssignedSpeedModification(player)
+
+    class AssignedSpeedModification(override val player: AmongUsPlayer) : AssignedAttributeModifier<SpeedModification, AssignedSpeedModification> {
+        override val definition = SpeedModification
+
+        override val attribute: Attribute = Attribute.MOVEMENT_SPEED
+        override val modifier = Companion.modifier
+
+        companion object {
+            private val key = NamespacedKey(AmongUs, "modification/$id")
+            private val modifier = AttributeModifier(key, 0.2, AttributeModifier.Operation.ADD_NUMBER)
+        }
+    }
+}
