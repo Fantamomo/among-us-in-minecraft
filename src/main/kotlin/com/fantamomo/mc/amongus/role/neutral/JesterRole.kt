bf7be13af4be9ce7f1b66808533ee39e298480f3
Fantamomo <git@fantamomo.com>
2026-03-08 11:53:21 +0100

    feat(role): add win condition for Jester role
    - Implemented `hasWon` logic for the Jester role based on being ejected.
    - Introduced `winCheckPhase` set to `PRE` for phase-specific win evaluation.
    - Removed direct win handling from `MeetingManager` for better role abstraction.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

--- src/main/kotlin/com/fantamomo/mc/amongus/role/neutral/JesterRole.kt
+++ src/main/kotlin/com/fantamomo/mc/amongus/role/neutral/JesterRole.kt
@@ -2,9 +2,11 @@ package com.fantamomo.mc.amongus.role.neutral
 
 import com.fantamomo.mc.amongus.ability.Ability
 import com.fantamomo.mc.amongus.player.AmongUsPlayer
+import com.fantamomo.mc.amongus.player.info.DeadReason
 import com.fantamomo.mc.amongus.role.AssignedRole
 import com.fantamomo.mc.amongus.role.Role
 import com.fantamomo.mc.amongus.role.Team
+import com.fantamomo.mc.amongus.role.util.WinCheckPhase
 
 object JesterRole : Role<JesterRole, JesterRole.AssignedJesterRole> {
     override val id: String = "jester"
@@ -15,5 +17,9 @@ object JesterRole : Role<JesterRole, JesterRole.AssignedJesterRole> {
 
     class AssignedJesterRole(override val player: AmongUsPlayer) : AssignedRole<JesterRole, AssignedJesterRole>{
         override val definition = JesterRole
+
+        override val winCheckPhase: WinCheckPhase = WinCheckPhase.PRE
+
+        override fun hasWon() = player.deadReason === DeadReason.Ejected
     }
 }
