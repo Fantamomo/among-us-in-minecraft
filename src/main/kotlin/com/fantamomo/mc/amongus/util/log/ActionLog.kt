51199a114eb67698bd14da881f9107ac74579bb8
Fantamomo <git@fantamomo.com>
2026-03-13 18:48:07 +0100

    feat(log): add ActionLog class to manage action entries
    - Introduced `ActionLog` for tracking and managing action entries with metadata.
    - Added support for logging entries with timestamps using `ActionEntry` and `Clock.System.now()`.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

---
+++ src/main/kotlin/com/fantamomo/mc/amongus/util/log/ActionLog.kt
@@ -0,0 +1,19 @@
+package com.fantamomo.mc.amongus.util.log
+
+import kotlin.time.Clock
+import kotlin.uuid.Uuid
+
+class ActionLog(
+    val id: Uuid,
+    var metadata: Map<String, Any> = emptyMap()
+) {
+    internal val entries: ArrayDeque<ActionEntry> = ArrayDeque()
+
+    fun addEntry(entry: ActionEntry) {
+        entries.add(entry)
+    }
+
+    fun add(type: ActionElement) {
+        addEntry(ActionEntry(type, Clock.System.now()))
+    }
+}
