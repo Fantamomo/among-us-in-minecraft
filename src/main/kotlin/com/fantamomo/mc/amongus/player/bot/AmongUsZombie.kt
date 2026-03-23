becd0f3c8e86ac6124c01a34afd74b9117e447f6
Fantamomo <git@fantamomo.com>
2026-03-23 09:39:11 +0100

    feat(bot): add `AmongUsZombie` class for customized zombie bot behavior
    - Implemented `AmongUsZombie` with tailored spawn settings and behavior overrides.
    - Added invulnerability, no-door-breaking, and silent movement for better integration into the game.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

---
+++ src/main/kotlin/com/fantamomo/mc/amongus/player/bot/AmongUsZombie.kt
@@ -0,0 +1,52 @@
+package com.fantamomo.mc.amongus.player.bot
+
+import com.fantamomo.mc.amongus.AmongUsConstants
+import com.fantamomo.mc.amongus.manager.EntityManager
+import net.minecraft.server.level.ServerLevel
+import net.minecraft.world.DifficultyInstance
+import net.minecraft.world.entity.Entity
+import net.minecraft.world.entity.EntitySpawnReason
+import net.minecraft.world.entity.EntityType
+import net.minecraft.world.entity.SpawnGroupData
+import net.minecraft.world.entity.monster.zombie.Zombie
+import net.minecraft.world.level.Level
+import net.minecraft.world.level.ServerLevelAccessor
+import org.bukkit.craftbukkit.CraftWorld
+import org.bukkit.event.entity.CreatureSpawnEvent
+
+class AmongUsZombie internal constructor(val controller: BotController, level: ServerLevel) : Zombie(EntityType.ZOMBIE, level) {
+
+    init {
+        val lobbySpawn = controller.player.game.area.lobbySpawn ?: throw IllegalStateException("Lobby spawn not found")
+        EntityManager.addEntityToRemoveOnEnd(controller.player.game, bukkitEntity)
+        isCustomNameVisible = false
+        isInvulnerable = true
+        isSilent = true
+        bukkitEntity.isVisibleByDefault = AmongUsConstants.IN_DEVELOPMENT
+        collides = false
+        persist = true
+        setCanPickUpLoot(false)
+        isBaby = false
+        lobbySpawn.run { setPos(blockX.toDouble() + 0.5, blockY.toDouble(), blockZ.toDouble() + 0.5) }
+        (controller.player.game.world as CraftWorld).addEntity<org.bukkit.entity.Zombie>(this as Entity, CreatureSpawnEvent.SpawnReason.CUSTOM, null, false)
+    }
+
+    override fun isSunSensitive(): Boolean = false
+
+    override fun canBreakDoors() = false
+
+    override fun isUnderWaterConverting() = false
+
+    override fun isPushable() = false
+
+    override fun createNavigation(level: Level) = BotPathNavigation(this, level)
+
+    override fun registerGoals() {}
+
+    override fun finalizeSpawn(
+        level: ServerLevelAccessor,
+        difficulty: DifficultyInstance,
+        spawnReason: EntitySpawnReason,
+        spawnGroupData: SpawnGroupData?
+    ): SpawnGroupData? = spawnGroupData
+}
