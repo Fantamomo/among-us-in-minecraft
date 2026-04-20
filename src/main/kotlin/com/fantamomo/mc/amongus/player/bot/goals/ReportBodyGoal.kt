2ce3355de232c89dafdf4e118c17c10b8e232e67
Fantamomo <git@fantamomo.com>
2026-04-20 14:08:46 +0200

    fix(goals): prevent cannibals from reporting bodies in `ReportBodyGoal`
    - Added `CannibalRole` and `EatBodyAbility` checks to `canUse` method.
    - Fixed logic issue with kill cooldown comparison in `canUse`.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

--- src/main/kotlin/com/fantamomo/mc/amongus/player/bot/goals/ReportBodyGoal.kt
+++ src/main/kotlin/com/fantamomo/mc/amongus/player/bot/goals/ReportBodyGoal.kt
@@ -1,8 +1,10 @@
 package com.fantamomo.mc.amongus.player.bot.goals
 
+import com.fantamomo.mc.amongus.ability.abilities.EatBodyAbility
 import com.fantamomo.mc.amongus.manager.MeetingManager
 import com.fantamomo.mc.amongus.player.AmongUsPlayer
 import com.fantamomo.mc.amongus.player.bot.AmongUsZombie
+import com.fantamomo.mc.amongus.role.neutral.CannibalRole
 import net.minecraft.core.BlockPos
 import net.minecraft.world.entity.ai.goal.target.TargetGoal
 import org.bukkit.craftbukkit.entity.CraftMannequin
@@ -17,8 +19,10 @@ class ReportBodyGoal(val zombie: AmongUsZombie) : TargetGoal(zombie, true), Cust
     private var ticks: Int = 0
 
     override fun canUse(): Boolean {
+        if (player.role.definition === CannibalRole) return false
+        if (player.hasAbility(EatBodyAbility)) return false
         val lastKillTime = player.lastKillTime
-        if (lastKillTime != null && lastKillTime + 30.seconds < Clock.System.now()) return false
+        if (lastKillTime != null && lastKillTime + 30.seconds > Clock.System.now()) return false
         findTarget()
         return targetBody != null
     }
