2c0564b8833a0c47573e32fd8822799d2c120183
Fantamomo <git@fantamomo.com>
2026-04-30 11:06:24 +0200

    docs(util): add KDoc for `MainServerThreadDispatcher` and `Dispatchers.ServerThread`
    - Documented the `MainServerThreadDispatcher` and its usage for scheduling tasks on the Minecraft server main thread, including lifecycle and scheduling details.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

--- src/main/kotlin/com/fantamomo/mc/amongus/util/coroutines/MainServerThreadDispatcher.kt
+++ src/main/kotlin/com/fantamomo/mc/amongus/util/coroutines/MainServerThreadDispatcher.kt
@@ -7,6 +7,9 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Runnable
 import kotlin.coroutines.CoroutineContext
 
+/**
+ * @see Dispatchers.ServerThread
+ */
 private object MainServerThreadDispatcher : CoroutineDispatcher() {
     override fun dispatch(context: CoroutineContext, block: Runnable) {
         AmongUs.server.scheduler.runTaskLater(AmongUs, block, 0L)
@@ -17,6 +20,31 @@ private object MainServerThreadDispatcher : CoroutineDispatcher() {
     override fun toString() = "MainServerThreadDispatcher"
 }
 
+/**
+ * Dispatcher for executing coroutine work on the Minecraft server main thread.
+ *
+ * All tasks are scheduled through the AmongUs plugin scheduler and therefore
+ * run inside the server tick loop.
+ *
+ * This dispatcher should be used for Bukkit/Paper APIs that must run on the
+ * main server thread.
+ *
+ * ## Important Notes
+ *
+ * - Execution is tick-based and not immediate.
+ * - Tasks may run later than expected depending on server TPS and scheduler load.
+ * - `delay(...)` is not guaranteed to be accurate because timing depends on ticks.
+ * - All scheduled tasks are bound to the AmongUs plugin lifecycle.
+ *
+ * ## Plugin Lifecycle
+ *
+ * Tasks scheduled through this dispatcher depend on the AmongUs plugin.
+ *
+ * If the plugin is disabled:
+ * - New tasks cannot be scheduled.
+ * - Pending tasks may never be executed.
+ * - Suspended coroutines waiting for this dispatcher may never resume.
+ */
 @Suppress("UnusedReceiverParameter")
 val Dispatchers.ServerThread: CoroutineDispatcher
     get() = MainServerThreadDispatcher
